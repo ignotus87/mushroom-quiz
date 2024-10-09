faa06481fe2eb470071ae96a4fc5bb4ddcc3cf55
Takacs, Zsolt <zsolt.takacs.ext@siemens-energy.com>
2024-10-09 18:57:16 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# mushroom-quiz
+
+## what's this?
+A fish quiz for the mushrooms of Hungary.
+
+## deployed app url
+https://ignotus87.github.io/mushroom-quiz/
+
+## users' manual
+
+This website contains test materials for mushroom species.
+
+
+## tests/quizes
+ * select the correct name out of 4, by the image of a species, with a means to redo last round's mistakes
+
+# development
+
+## start local web server
+
+You'll need node.js.
+
+Installing the Node.js http-server is simple.
+
+npm install http-server -g
+
+This will install the http-server package globally. Now we can start the server to serve the files in the current directory.
+
+http-server . -p 8000
---
+++ selectNameByImage.js
@@ -0,0 +1,259 @@
+const speciesImport = import("./Species/speciesList.json", {
+    with: { type: 'json' }
+});
+
+speciesImport.then(data => {
+
+    const { createApp } = Vue
+
+    createApp({
+        data(speciesList) {
+            return {
+                title: 'Magyarország gombái',
+                speciesList: data.default,
+                puzzleItems: data.default,
+                puzzle: null,
+                incorrect1: '',
+                incorrect2: '',
+                incorrect3: '',
+                comment: '',
+                actualIndex: 1,
+                totalPoints: 0,
+                imageIndex: 0,
+                numberOfAnsweredQuestions: 0,
+                numberOfCorrectAnswers: 0,
+                previousPuzzleIDs: [],
+                game: 'SelectNameByImage',
+                choices: [],
+                choiceIsRight: [false, false, false, false],
+                choiceIsWrong: [false, false, false, false],
+                isAnswered: false,
+                mistakeIndexes: [],
+                gameType: 'all',
+                isWrongAnswer: false,
+                continueTimeoutHandle: null,
+                timeoutSeconds: 0
+            }
+        },
+        computed: {
+            totalSpeciesCount() {
+                return this.speciesList.length;
+            },
+            totalPuzzleItemsCount() {
+                return this.puzzleItems.length;
+            },
+            puzzleName() {
+                return this.puzzle.Name;
+            },
+            puzzleImage() {
+                return "./SpeciesImages/" + this.puzzle.ID + (this.imageIndex === 0 ? "" : "_" + (this.imageIndex + 1).toString()) + ".jpg";
+            },
+            commentToShow() {
+                return this.comment;
+            },
+            correctRatio() {
+                if (this.numberOfAnsweredQuestions === 0) {
+                    return '-';
+                }
+                else {
+                    return (100 * this.numberOfCorrectAnswers / this.numberOfAnsweredQuestions).toFixed() + "%";
+                }
+            },
+            imageSource() {
+                let temp = this.puzzle.ImageSource[this.imageIndex].replace('https://', '').replace('http://', '');
+                if (temp.indexOf('/') > 0) {
+                    temp = temp.substring(0, temp.indexOf('/'))
+                }
+                return temp;
+            },
+            gameTitle() {
+                if (this.game === "SelectNameByImage") {
+                    if (this.gameType === 'all') {
+                        return "Melyik gombafajt látod a képen?";
+                    } else {
+                        return "Hibásak gyakorlása: Melyik gombafajt látod a képen?";
+                    }
+                }
+            },
+            isRightAnswer() {
+                return !this.isWrongAnswer;
+            }
+        },
+        methods: {
+            getRandomSpecies() {
+                if (this.previousPuzzleIDs.length == this.puzzleItems.length) { return this.puzzle; }
+
+                do {
+                    var candidate = this.puzzleItems[Math.floor(Math.random() * this.puzzleItems.length)];
+                } while (this.previousPuzzleIDs.some((id) => candidate.ID === id));
+
+                this.previousPuzzleIDs.push(candidate.ID);
+
+                return candidate;
+            },
+            getRandomSpeciesFromPuzzleItemsExceptIDs(ids) {
+                do {
+                    var result = this.puzzleItems[Math.floor(Math.random() * this.puzzleItems.length)];
+                } while (ids.some((id) => result.ID === id))
+                return result;
+            },
+            getRandomSpeciesFromAllItemsExceptIDs(ids) {
+                do {
+                    var result = this.speciesList[Math.floor(Math.random() * this.speciesList.length)];
+                } while (ids.some((id) => result.ID === id))
+                return result;
+            },
+            getIncorrectAnswers() {
+
+                if (this.puzzle.SimilarSpecies.length >= 1) {
+                    this.incorrect1 = this.speciesList.find((item) => item.ID === this.puzzle.SimilarSpecies[0]);
+                }
+                else {
+                    this.incorrect1 = this.getRandomSpeciesFromAllItemsExceptIDs([this.puzzle.ID]);
+                }
+
+                if (this.puzzle.SimilarSpecies.length >= 2) {
+                    this.incorrect2 = this.speciesList.find((item) => item.ID === this.puzzle.SimilarSpecies[1]);
+                }
+                else {
+                    this.incorrect2 = this.getRandomSpeciesFromAllItemsExceptIDs([this.puzzle.ID, this.incorrect1.ID]);
+                }
+
+                if (this.puzzle.SimilarSpecies.length >= 3) {
+                    this.incorrect3 = this.speciesList.find((item) => item.ID === this.puzzle.SimilarSpecies[2]);
+                }
+                else {
+                    this.incorrect3 = this.getRandomSpeciesFromAllItemsExceptIDs([this.puzzle.ID, this.incorrect1.ID, this.incorrect2.ID]);
+                }
+
+            },
+            shuffle(array) {
+                let currentIndex = array.length, randomIndex;
+
+                // While there remain elements to shuffle.
+                while (currentIndex != 0) {
+
+                    // Pick a remaining element.
+                    randomIndex = Math.floor(Math.random() * currentIndex);
+                    currentIndex--;
+
+                    // And swap it with the current element.
+                    [array[currentIndex], array[randomIndex]] = [
+                        array[randomIndex], array[currentIndex]];
+                }
+
+                return array;
+            },
+            isCorrect(answer) {
+                return answer === this.puzzleName;
+            },
+            answered(answer) {
+                if (this.isAnswered) {
+                    // It's a second click, it should be a "continue" trigger
+                    this.continueAfterAnswer();
+                    return;
+                }
+
+                this.isAnswered = true;
+                var indexOfAnswer = this.choices.indexOf(answer);
+
+                if (this.isCorrect(answer)) {
+                    this.comment = '<i>' + this.puzzle.Category + '</i><br/>' + this.puzzle.DistinctionInfo;
+                    this.choiceIsRight[indexOfAnswer] = true;
+                    this.totalPoints++;
+                    this.numberOfCorrectAnswers++;
+
+                    this.timeoutSeconds = 6;
+                }
+                else {
+                    this.comment = '<i>' + this.puzzle.Category + '</i><br/>' + this.puzzle.DistinctionInfo;
+                    this.choiceIsWrong[indexOfAnswer] = true;
+                    this.mistakeIndexes.push(this.puzzle.ID);
+                    var indexOfCorrectAnswer = this.choices.indexOf(this.puzzleName);
+                    this.choiceIsRight[indexOfCorrectAnswer] = true;
+                    this.isWrongAnswer = true;
+
+                    this.timeoutSeconds = 12;
+                }
+
+                this.continueTimeoutHandle = setTimeout(() => this.countdownToContinue(), 1000);
+
+                ++this.numberOfAnsweredQuestions;
+
+
+            },
+            countdownToContinue() {
+                if (this.timeoutSeconds > 0) {
+                    this.timeoutSeconds--;
+                    this.continueTimeoutHandle = setTimeout(() => this.countdownToContinue(), 1000);
+                }
+                else {
+                    this.continueAfterAnswer();
+                }
+            },
+            nextPuzzle() {
+                this.puzzle = this.getRandomSpecies();
+                this.imageIndex = Math.floor(Math.random() * this.puzzle.ImageSource.length);
+                this.getIncorrectAnswers();
+                this.randomizeChoices();
+                this.resetChoiceColors();
+                this.actualIndex++;
+                this.isAnswered = false;
+                this.isWrongAnswer = false;
+            },
+            startQuiz() {
+                this.previousPuzzleIDs = [];
+                this.actualIndex = 1;
+                this.totalPoints = 0;
+                this.numberOfCorrectAnswers = 0;
+                this.numberOfAnsweredQuestions = 0;
+                this.puzzle = this.getRandomSpecies();
+                this.getIncorrectAnswers();
+                this.randomizeChoices();
+                this.resetChoiceColors();
+                this.comment = '';
+                this.isAnswered = false;
+                this.isWrongAnswer = false;
+            },
+            reset() {
+                this.gameType = 'all';
+                this.puzzleItems = this.speciesList;
+                this.startQuiz()
+                this.mistakeIndexes = [];
+            },
+            restartWithMistakes() {
+                this.gameType = 'mistakesOnly';
+                this.puzzleItems = this.speciesList.filter((item) => this.mistakeIndexes.includes(item.ID));
+                this.startQuiz()
+                this.mistakeIndexes = [];
+            },
+            endGame() {
+                this.comment = "Vége a játéknak! Eredmény: " + Math.floor(this.totalPoints / this.numberOfAnsweredQuestions * 100) + '%';
+            },
+            randomizeChoices() {
+                this.choices = this.shuffle([this.puzzleName, this.incorrect1.Name, this.incorrect2.Name, this.incorrect3.Name]);
+            },
+            resetChoiceColors() {
+                this.choiceIsRight = [false, false, false, false];
+                this.choiceIsWrong = [false, false, false, false];
+            },
+            continueAfterAnswer() {
+
+                if (this.continueTimeoutHandle != null) {
+                    clearTimeout(this.continueTimeoutHandle);
+                }
+
+                if (this.previousPuzzleIDs.length >= this.totalPuzzleItemsCount) {
+                    this.endGame();
+                }
+                else {
+                    this.nextPuzzle();
+                    this.comment = '';
+                }
+            }
+        },
+        created() {
+            this.startQuiz()
+        }
+    }).mount('#app')
+});
