52909d56e4f5c09a30e872b8b39639a72047e995
Takacs, Zsolt <zsolt.takacs.ext@siemens-energy.com>
2024-12-26 09:12:43 +0100

    Make cache valid for 2 hours

--- serviceWorker.js
+++ serviceWorker.js
@@ -16,15 +16,52 @@ self.addEventListener('activate', (event) => {
     console.log('Service worker activate event!');
 });
 
+// Caching approach based on https://gomakethings.com/how-to-set-an-expiration-date-for-items-in-a-service-worker-cache/
+
+/**
+* Check if cached API data is still valid
+* @param  {Object}  response The response object
+* @return {Boolean}          If true, cached data is valid
+*/
+var isValid = function (response) {
+    if (!response) return false;
+    var fetched = response.headers.get('sw-fetched-on');
+    if (fetched && (parseFloat(fetched) + (1000 * 60 * 60 * 2)) > new Date().getTime()) return true;
+    return false;
+};
+
 // When there's an incoming fetch request, try and respond with a precached resource, otherwise fall back to the network
 self.addEventListener('fetch', (event) => {
     console.log('Fetch intercepted for:', event.request.url);
     event.respondWith(
         caches.match(event.request).then((cachedResponse) => {
-            if (cachedResponse) {
+            if (isValid(cachedResponse)) {
                 return cachedResponse;
             }
-            return fetch(event.request);
+            return fetch(event.request).then(function (response) {
+
+                // Cache for offline access
+                var copy = response.clone();
+                event.waitUntil(caches.open('api').then(function (cache) {
+                    var headers = new Headers(copy.headers);
+                    headers.append('sw-fetched-on', new Date().getTime());
+                    return copy.blob().then(function (body) {
+                        return cache.put(event.request, new Response(body, {
+                            status: copy.status,
+                            statusText: copy.statusText,
+                            headers: headers
+                        }));
+                    });
+                }));
+
+                // Return the requested file
+                return response;
+
+            }).catch(function (error) {
+                return caches.match(event.request).then(function (response) {
+                    return response || caches.match('/offline.json');
+                });
+            });
         }),
     );
 });
